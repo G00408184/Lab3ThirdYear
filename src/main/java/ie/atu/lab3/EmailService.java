c276e6aa27c74d23ba4a47089171f65233243012
Jakub Mamala <G00408184@atu.ie>
2023-10-10 15:33:05 +0100

    Created the constructors in email service

--- src/main/java/ie/atu/lab3/EmailService.java
+++ src/main/java/ie/atu/lab3/EmailService.java
@@ -1,4 +1,19 @@
 package ie.atu.lab3;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmailService {
+    private  final EmailService emailService;
+
+    @Autowired
+
+    public EmailService(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    public void registerUser(String username,String email){
+
+    }
 }
