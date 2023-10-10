4786f8a999e80724a5c136a673936a8ed7be4a1f
Jakub Mamala <G00408184@atu.ie>
2023-10-10 16:46:57 +0100

    made the controller class and finished

--- src/main/java/ie/atu/lab3/part3/LibraryController.java
+++ src/main/java/ie/atu/lab3/part3/LibraryController.java
@@ -1,4 +1,20 @@
 package ie.atu.lab3.part3;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@AllArgsConstructor
 public class LibraryController {
+    LibraryService libraryService;//this makes this class able to access from LibraryService
+
+    @GetMapping("/book{")
+    public ArrayList<Book> getBook(){
+        return libraryService.retrieve();
+    }
+
 }
