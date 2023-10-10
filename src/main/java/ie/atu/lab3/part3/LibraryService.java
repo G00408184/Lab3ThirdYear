95cc848d986cdc5fab75f7c02ebca769a1d23626
Jakub Mamala <G00408184@atu.ie>
2023-10-10 16:26:40 +0100

    made an array list

--- src/main/java/ie/atu/lab3/part3/LibraryService.java
+++ src/main/java/ie/atu/lab3/part3/LibraryService.java
@@ -1,47 +1,16 @@
 package ie.atu.lab3.part3;
 
+import org.springframework.stereotype.Service;
+
+import java.sql.Array;
+import java.sql.ClientInfoStatus;
+import java.util.ArrayList;
+
+@Service
 public class LibraryService {
-    private String title;
-    private String author;
-    private String isbn;
-    private int publishedYear;
-
-    public LibraryService(String title, String author, String isbn, int publishedYear) {
-        this.title = title;
-        this.author = author;
-        this.isbn = isbn;
-        this.publishedYear = publishedYear;
+    ArrayList<Book> list = new ArrayList<>();
+    public void add(Book book){
+        list.add(book);
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public int getPublishedYear() {
-        return publishedYear;
-    }
-
-    public void setPublishedYear(int publishedYear) {
-        this.publishedYear = publishedYear;
-    }
 }
